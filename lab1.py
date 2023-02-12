066e0089fdbccdad7d82151ea0098c6e1e20c240
Gartur1337 <gakiev13@mail.ru>
2023-02-12 21:43:00 +0300

    update

--- lab1.py
+++ lab1.py
@@ -10,14 +10,15 @@ def main():
     w = [random.uniform(0,1) for i in range(4)]
     data_set = [[] for i in range(4)]
     normalization_data_set = [[] for i in range(4)]
-    print(w,'\n')
+
+    print('weigths:', w, '\n')
 
     excel_data = pd.read_excel('lab1.xlsx')
 
     data = pd.DataFrame(excel_data, columns=['x1', 'x2', 'x3', 'y'])
 
     data_set = [list(v) for k, v in data.items()]
-
+    
     for i in range(len(data_set)):
         for j in range(len(data_set[i])):
             normalization_data_set[i].append((data_set[i][j] - min(data_set[i])) / (max(data_set[i]) - min(data_set[i])))
@@ -25,39 +26,44 @@ def main():
     for set in normalization_data_set:
         print(set,'\n')
 
-    for _ in range(20):
+    for epoch in range(200):
         s = 0
         delta = 0
-        v = 0.5
+        v = 0.9
         ans = []
         delta_array = []
-
+        result = 0
         for k in range(14):
             for i in range(len(w)):
                 if i == 0:
                     s += w[i] * 1
                 else:
-                    s += w[i] * normalization_data_set[i][k]
+                    s += w[i] * normalization_data_set[i-1][k]
 
-            f = 1 / (1 + math.exp(-0.1 * s))
-
+            f = 1 / (1 + math.exp(-1 * s))
             delta = normalization_data_set[-1][k] - f
-    
+
             for i in range(len(w)):
                 if i == 0:
+                    # print('weight:', i, w[i], '\n')
                     w[i] += delta * v
                 else:
-                    w[i] += delta * v * normalization_data_set[i][k]      
-            
+                    # print('weight:', i, w[i], '\n', 'data set:', normalization_data_set[i-1][k])
+                    w[i] += delta * v * normalization_data_set[i-1][k]      
             ans.append(f)
             delta_array.append(delta)
-        
-        for set in normalization_data_set:
-            shuffle(set)
-            
 
-    print("answer ", ans, '\n'*2, "delta", delta_array)    
+        for k in range(14):
+            normalization_data_set[-1][k] = math.sin(normalization_data_set[0][k]) * (normalization_data_set[1][k] + normalization_data_set[2][k])
 
+        for i in range(len(normalization_data_set) - 1):
+            shuffle(normalization_data_set[i])
+
+        result += math.pow(normalization_data_set[-1][k] - f,2)
+        e = math.sqrt(result * (1 / 14))
+        print("Error on epoch: ", epoch, e)
+
+    # print("answer ", ans, '\n'*2, "delta", delta_array)    
 
 if __name__ == '__main__':
     main()
